2d6df7b056146f2aeb59e308430fb18339dd864a
roud <AN16720@ens.etsmtl.ca>
2019-02-01 23:52:55 -0500

    add new pages

---
+++ src/pages/newpage2/newpage2.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Newpage2Page } from './newpage2';
+
+@NgModule({
+  declarations: [
+    Newpage2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Newpage2Page),
+  ],
+})
+export class Newpage2PageModule {}
