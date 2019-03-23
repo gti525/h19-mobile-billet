2436fbcaaa68617dac982332fff8d3ce5faf6758
Luc Delplanque <delplanqueluc@gmail.com>
2019-03-23 17:31:34 -0500

    friends displayed

--- src/app/pages/event/event.page.ts
+++ src/app/pages/event/event.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { TicketsService } from '../../services/tickets.service';
-import { Router } from '@angular/router';
+import { EventService } from 'src/app/services/event.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 @Component({
@@ -10,18 +10,93 @@ import { Router } from '@angular/router';
 })
 export class EventPage implements OnInit {
 
-  private ticketList = [];
+    friendList: any;
 
-    constructor( private ticketService: TicketsService, private router: Router ) {
-        this.ticketList = [
-            { event: 'Cirque du Soleil', date: 'Lundi 21 janvier', seat: '3A', localisation: 'Centre Bell', id: 'string1' },
-            { event: 'Concert Celine Dion', date: 'Venredi 14 mars', seat: '3A', localisation: 'Centre Bell', id: 'string2' },
-            { event: 'Festival', date: 'Jeudi 12 mai', seat: '3A', localisation: 'Centre Bell', id: 'string3' }
-        ];
+    constructor( private eventService: EventService, private http: HttpClient ) { }
+
+    ngOnInit() {
+        this.getFriends();
+        this.eventService.getFriends()
+            .then(value => {
+                if (value) {
+                    this.friendList = value;
+                }
+                else this.getFriends();
+            })
+            .catch(() => this.getFriends());
+        // this.friendList = [
+        //     {
+        //         ClientId: 1,
+        //         FirstName: "Bob",
+        //         Tickets: [
+        //             {
+        //                 "Id": 45,
+        //                 "UUID": 54677,
+        //                 "EventName": "ROUGE",
+        //                 "Artist": "Barbe bleue",
+        //                 "Date": "2019-05-25T00:00:00",
+        //                 "Location": "Mtl",
+        //                 "ClientId": 0,
+        //                 "Client": null
+        //             },
+        //             {
+        //                 "Id": 46,
+        //                 "UUID": 65744,
+        //                 "EventName": "BLEU",
+        //                 "Artist": "Barbe bleue",
+        //                 "Date": "2019-05-25T00:00:00",
+        //                 "Location": "Mtl",
+        //                 "ClientId": 0,
+        //                 "Client": null
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         ClientId: 2,
+        //         FirstName: "Alice",
+        //         Tickets: [
+        //             {
+        //                 "Id": 45,
+        //                 "UUID": 54677,
+        //                 "EventName": "ORANGE",
+        //                 "Artist": "Barbe bleue",
+        //                 "Date": "2019-05-25T00:00:00",
+        //                 "Location": "Mtl",
+        //                 "ClientId": 0,
+        //                 "Client": null
+        //             },
+        //             {
+        //                 "Id": 46,
+        //                 "UUID": 65744,
+        //                 "EventName": "VIOLET",
+        //                 "Artist": "Barbe bleue",
+        //                 "Date": "2019-05-25T00:00:00",
+        //                 "Location": "Mtl",
+        //                 "ClientId": 0,
+        //                 "Client": null
+        //             }
+        //         ]
+        //     }
+        // ]
     }
 
-  ngOnInit() {
+    getFriends () {
+        const headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyIiwibmJmIjoxNTUyOTI4Mjc4LCJleHAiOjE1NTM1MzMwNzgsImlhdCI6MTU1MjkyODI3OH0.LPaMQEVXF_MC5MbNJvwmuh84crbGdEbZx4ktDnvuJFc`
+        })
 
-  }
+        this.http.get('https://core-api-525.herokuapp.com/api/Client/friend', { headers })
+            .subscribe(data => {
+                console.log(JSON.stringify(data));
+                this.friendList = data;
+                this.eventService.saveFriends(data)
+                    .then(() => console.log("friends saved") )
+                    .catch(err => console.log(err))
+            }, error => {
+                console.log("could not get ticket");
+                console.log(error);
+            })
+    }
 
 }
--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
-  constructor() { }
+    friendList: any;
+    FRIEND_INFO = "friendKey";
+
+    constructor(private storage: Storage) { }
+
+    getFriends () {
+        return this.storage.get(this.FRIEND_INFO);
+    }
+
+    saveFriends (friendList) {
+        return this.storage.set(this.FRIEND_INFO,friendList);
+    }
+
+    deleteFriends () {
+        return this.storage.set(this.FRIEND_INFO, null);
+    }
 }
